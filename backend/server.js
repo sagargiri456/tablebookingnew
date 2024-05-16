f45e64cd3fa0be7e857ce7454770d6a151401a8e
Sagar Giri <sagargiri456@gamil.com>
2024-05-16 09:59:30 -0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
+
+mongoose.connect("mongodb://localhost:27017/FoodOrdering", () =>
+  console.log("Database connected")
+);
+
+app.use(express.json());
+app.use(cors());
+app.use("/app", routesUrls);
+
+app.listen(4000, () => console.log("server is up and running"));
