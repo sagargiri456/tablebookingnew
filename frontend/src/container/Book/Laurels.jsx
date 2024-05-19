c3a544e5211f6df9218bdbf10f95d0a55e0162ff
sagargiri456 <135608808+sagargiri456@users.noreply.github.com>
2024-05-18 22:30:49 -0700

    Update Laurels.jsx

--- frontend/src/container/Book/Laurels.jsx
+++ frontend/src/container/Book/Laurels.jsx
@@ -23,7 +23,7 @@ const Book = () => {
       bookingtime,
     };
     axios
-      .post("https://tablebookingnew.onrender.com/book", booked)
+      .post("https://tablebookingnew.onrender.com/app/book", booked)
       .then((response) => console.log(response.data));
     alert("Your booking has been confirmed");
     window.location = "/";
