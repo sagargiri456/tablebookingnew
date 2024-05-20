71f120ce063ff0e2ac55bc92b1394cebac2dabff
sagargiri456 <135608808+sagargiri456@users.noreply.github.com>
2024-05-20 00:57:02 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ const mongoose = require("mongoose");
 const routesUrls = require("./routes/routes");
 const cors = require("cors");
 
-mongoose.connect(
-  "mongodb+srv://sagargiri456:pass%40123@cluster0.la3f9jt.mongodb.net/FoodOrder?retryWrites=true&w=majority&appName=Cluster0"
-);
-// app.get("/", (req, res) => {
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.send("api is running");
-// });
+mongoose
+  .connect(
+    "mongodb+srv://sagargiri456:pass%40123@cluster0.la3f9jt.mongodb.net/FoodOrder?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log("Database connection error: " + err));
+
 
 app.use(express.json());
 app.use(cors());
