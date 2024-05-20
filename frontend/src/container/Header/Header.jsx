64126f34253b0cad7f366ccad0c6514a2c588d4d
sagargiri456 <135608808+sagargiri456@users.noreply.github.com>
2024-05-20 09:58:48 -0700

    Update Header.jsx

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="Broaden your horizons" />
-      <h1 className="app__header-h1">An culinary experience like never before</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>We are fanatically meticulous and particular about the food we serve. The menu offers range which is wide enough for you to find something that appeals to your specific palate. </p>
+      <h1 className="app__header-h1">A unique flavor experience that will amaze your senses.</h1>
+      <p className="p__opensans" style={{ margin: '2rem 0' }}>We are obsessively detailed and selective about the food we serve. Our menu is diverse enough for you to discover something that perfectly suits your taste. </p>
       <Link to='/menu'><button type="button" className="custom__button">View Menu</button></Link>
     </div>
 
